f890cbfde45b1f51ad5216cfe88eeb1fe83d0a3b
Maxim Tolochko <maksim.tolo@gmail.com>
2016-01-11 16:46:20 +0300

    bug fix

--- js/neural-network-worker.js
+++ js/neural-network-worker.js
@@ -2,9 +2,13 @@ importScripts('../bower_components/sylvester/sylvester.js', './neural-network.js
 
 class NeuralNetworkWorker {
   constructor(error, size, imageSize, secondLayerSize, vectors) {
+    const CHANNELS_NUMBER = 4;
     let iterations = 0;
     let currentError;
     let recirculationNeuralNetwork = new RecirculationNeuralNetwork(size, secondLayerSize);
+    let n = CHANNELS_NUMBER * size;
+    let l = imageSize / size;
+    let z = ((n + l) * secondLayerSize + 2) / (n * l + 2);
 
     do {
       this.teachNeuroWeb(vectors, recirculationNeuralNetwork);
@@ -12,9 +16,12 @@ class NeuralNetworkWorker {
       iterations++;
       console.log(iterations, currentError);
     } while(isNaN(currentError) || currentError > error);
-    let n = 3 * size;
-    let z = (n * imageSize) / ((imageSize + n) * secondLayerSize + 2);
-    console.log(z);
+
+    console.log('Compression ratio:', z);
+    console.log('Rectangles number:', l);
+    console.log('Form length:', n);
+    console.log('Total error:', currentError);
+    console.log('Number of iterations:', iterations);
     vectors.forEach((vector, i) => vector.forEach((element, j) => vectors[i][j] = recirculationNeuralNetwork.kick(element)));
     self.postMessage({
       vectors: vectors
